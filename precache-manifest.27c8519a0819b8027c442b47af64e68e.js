5babddabdf0ae370f7c55810afd0fd896f4eeba0
worrryfree <shinjw21@gmail.com>
2020-07-29 17:32:14 +0900

    Updates

---
+++ precache-manifest.27c8519a0819b8027c442b47af64e68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b17e20444f7a478017e114a6b28f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f279a54c1b1bca7cbf8f",
+    "url": "/static/css/main.408b9ef7.chunk.css"
+  },
+  {
+    "revision": "a0054564cd7082ef0d3c",
+    "url": "/static/js/2.59df1732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59df1732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f279a54c1b1bca7cbf8f",
+    "url": "/static/js/main.013cff8f.chunk.js"
+  },
+  {
+    "revision": "cf27343b5d386281fb60",
+    "url": "/static/js/runtime-main.1e9c0366.js"
+  }
+]);
